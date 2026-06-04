a0e219b5fd1960b278f78621f252db0122c44abb
iripurichan <irisu0917@gmail.com>
2026-06-04 12:34:31 +0900

    update index.html

---
+++ script.js
@@ -0,0 +1,56 @@
+let age = 16;
+
+conosole.log("あなたは"+age+"歳です")
+
+let score = 75
+
+if (score >= 70){
+
+console.log("合格")
+
+}
+else {
+
+console.log("不合格")
+
+}
+
+function Hello (){
+    console.log("こんにちは！")
+}
+
+function double(nombre){
+
+    console.log(nombre*2)
+}
+
+double (5)
+
+function check(number){
+
+    if (number % 2 === 0){
+        console.log("偶数")
+    }
+
+
+    else{
+
+        console.log("奇数")}
+}
+
+function attack(){
+
+    let hp = 30
+
+    while (hp > 0){
+
+        hp -= 10
+
+    }
+
+    if(hp = 0){
+
+        console.log("モンスターを倒した！")
+    }
+}
+
