2ec01d88cca3fc051dd2dd61cfc8d954aa26a9a7
iripurichan <irisu0917@gmail.com>
2026-06-05 10:43:47 +0900

    update index.hrml

--- script.js
+++ script.js
@@ -1,56 +1,11 @@
-let age = 16;
 
-conosole.log("あなたは"+age+"歳です")
+function changement_position {
 
-let score = 75
+    const element = index.htmel.getElementById("title_music");
 
-if (score >= 70){
+    
 
-console.log("合格")
-
-}
-else {
-
-console.log("不合格")
-
-}
-
-function Hello (){
-    console.log("こんにちは！")
-}
-
-function double(nombre){
-
-    console.log(nombre*2)
-}
-
-double (5)
-
-function check(number){
-
-    if (number % 2 === 0){
-        console.log("偶数")
-    }
-
-
-    else{
-
-        console.log("奇数")}
-}
-
-function attack(){
-
-    let hp = 30
-
-    while (hp > 0){
-
-        hp -= 10
-
-    }
-
-    if(hp = 0){
-
-        console.log("モンスターを倒した！")
-    }
+    
+    
 }
 
