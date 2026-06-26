80ccf1c648853241535388805951590efdc5a078
iripurichan <irisu0917@gmail.com>
2026-06-26 23:17:11 +0900

    update script.js

--- script.js
+++ script.js
@@ -1,25 +1,30 @@
-let shapeColor1
-let mode = 0
-//let shapeColor2
+let mode = 0;
+let miku;
 
+function preload(){
+
+    miku = loadImage('mikuchan.png'); 
+}
 
 function setup() {
 
     createCanvas(1500,775);
 
-    
+
 
 
 }
 
 function draw() {
 
-    background(255);
+    background(0);
 
     if(mode == 0){
 
-        fill(0);
-        circle(750,380,500);
+        fill(255);
+        circle(750,387.5,500);
+
+        image(miku,650,287.5);
 
 
     }
