ce1c311a88b03b836d1be355da17a0ffe05ebf0c
iripurichan <irisu0917@gmail.com>
2026-06-26 16:58:02 +0900

    script.js

--- script.js
+++ script.js
@@ -7,28 +7,6 @@ function setup() {
 
     createCanvas(1500,775);
 
-    colors1 = [
-
-    color(182, 220, 254),
-    color(129, 247, 229),
-    color(7, 113, 135),
-    color(83, 89, 154)
-
-    ];
-    shapeColor1 = random(colors1);
-
-    //colors2 = [
-
-    //color(205, 180, 219),
-   // color(255, 175, 204),
-    //color(213, 93, 146),
-    //color(228, 63, 111)
-
-
-   // ];
-
-    //shapeColor2 = random(colors2);
-
 
 }
 
@@ -38,48 +16,14 @@ function draw() {
 
     if(mode == 0){
 
-        fill(189, 224, 254);
+        fill(0);
         square(325,175,575);
 
         fill(255);
         quad(250,195,300,195,300,245,250,245)
-        
-    }
-
-
-    if (mode == 1){
-
-        fill(shapeColor1);
-        square(464,200,287.5);
-
-        fill(164, 195, 178);
-        square(751.5,200,287.5);
-
-        fill(244, 226, 133);
-        square(464,487.5,287.5);
-
-        fill(234, 242, 215);
-        square(751.5,487.5,287.5);
-    
-    }
-
-    if (mode == 2){
-
-        fill(255, 200, 221);
-        square(325,175,300);
-
 
     }
 
-    if (frameCount > 300){
-
-        mode = 1;
-    }
-
-    if (frameCount > 600){
-
-        mode = 2;
-    }
  }
 
     
